7541c709e674be4c984257dafbf32182feed5793
Samuel <diogosamuelfernandes@outlook.com>
2020-04-01 13:35:30 +0100

    Cylinder without base

--- source/MyCylinder.js
+++ source/MyCylinder.js
@@ -1,6 +1,6 @@
 /**
  * MyCylinder
- * @constructor
+ * @method constructor
  * @param scene - Reference to MyScene object
  * @param slices - Reference to number of sides of Cylinder
  */
@@ -12,18 +12,19 @@ class MyCylinder extends CGFobject {
   	}
 
   	initBuffers() {
-      this.vertices = [0, 0, 0]; // Base Center
-      this.normals = [0, -1, 0]; // Bas Normal -> If Base is necessary do it for each vertice at base
+      this.vertices = [];
+      this.indices = [];
+      this.normals = [];
       this.texCoords = [];
 
       var amplitude = (2 * Math.PI)/this.slices;
       var angle = 0;
-      var inc = 1;
 
       for (var i = 0; i < this.slices; i++) {
           var x = Math.cos(angle);
           var z = Math.sin(angle);
 
+          // Vertices of Cylinder
           // Base of Cylinder
           this.vertices.push(x);
           this.vertices.push(0);
@@ -34,10 +35,12 @@ class MyCylinder extends CGFobject {
           this.vertices.push(1);
           this.vertices.push(z);
 
+          // Normals
           // cos^2 (x) + sin^2 (x) = 1, so normal is unitary
           this.normals.push(Math.cos(angle), 0, Math.sin(angle));
           this.normals.push(Math.cos(angle), 0, Math.sin(angle));
 
+          // Texture Coordinates
           /*  0 ----------- 1
            *  |
            *  |
@@ -52,21 +55,15 @@ class MyCylinder extends CGFobject {
           angle += amplitude;
       }
 
-      this.indices = [];
+      // Draw Sides
+      for (var i = 0; i < this.slices * 2; i = i + 2) {
+          this.indices.push(i % (this.slices * 2));
+          this.indices.push((i + 1) % (this.slices * 2));
+          this.indices.push((i + 2) % (this.slices * 2));
 
-      for (var i = 0; i < this.slices * 2; i = i+2) {
-          // Cylinder bot view -> Is this necessary?
-          this.indices.push(1 + (i+2) % (this.slices * 2));
-          this.indices.push(0);
-          this.indices.push(1 + i);
-
-          this.indices.push(1 + i % (this.slices * 2));
-          this.indices.push(1 + (i + 1) % (this.slices * 2));
-          this.indices.push(1 + (i + 2) % (this.slices * 2));
-
-          this.indices.push(1 + (i + 3) % (this.slices * 2));
-          this.indices.push(1 + (i + 2) % (this.slices * 2));
-          this.indices.push(1 + (i + 1) % (this.slices * 2));
+          this.indices.push((i + 3) % (this.slices * 2));
+          this.indices.push((i + 2) % (this.slices * 2));
+          this.indices.push((i + 1) % (this.slices * 2));
       }
 
   		this.primitiveType = this.scene.gl.TRIANGLES;
