20bae6f3f91943aa0aac7211f0b5dd05eae13c7b
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2020-04-24 00:21:22 +0100

    Animation almost done

--- source/MyVehicle.js
+++ source/MyVehicle.js
@@ -12,6 +12,10 @@ class MyVehicle extends CGFobject {
           this.posZ = 0;
           this.auto = false;
           this.zeppelin = new MyZeppelin(scene, 5, 1);
+
+          //
+          this.first = false;
+          this.final = 0;
     }
     origDist() {
         return Math.sqrt(this.posX*this.posX + this.posZ*this.posZ);
@@ -19,19 +23,31 @@ class MyVehicle extends CGFobject {
     update() {
         if(this.auto) {
             if(4.9 < this.origDist() && this.origDist() < 5.1) {
-                this.angleY += (Math.PI*2) / 300;
-                this.velocity = 0;
-                this.posX = -5 * Math.cos(this.angleY);
-                this.posZ = 5 * Math.sin(this.angleY); 
+                if(this.first) {
+                    this.final = this.angleY + Math.PI / 2;
+                    this.first = false;
+                }
+                if(Math.abs(this.angleY - this.final) > 0.1) {
+                    this.angleY += Math.PI / 90;
+                }
+                else {
+                    this.angleY += (Math.PI*2) / 300;
+                    this.final = this.angleY;
+                    this.velocity = 10;
+                    this.posX = -5 * Math.cos(this.angleY);
+                    this.posZ = 5 * Math.sin(this.angleY); 
+                }
             }
             else if(this.origDist() < 5) {
                 this.posZ += Math.cos(this.angleY) * 0.01;
                 this.posX += Math.sin(this.angleY) * 0.01;
+                this.first = true;
             }
             else {
-
-            }
-            
+                //this.angleY = (this.posZ > 0) ? Math.PI/2 + Math.atan(this.posZ/this.posX) : Math.PI/2 + Math.atan(this.posZ/this.posX);
+                //this.posZ -= this.posZ * 0.01;
+                //this.posX -= this.posX * 0.01;
+            }   
         }
         else {
             this.posX += Math.sin(this.angleY) * this.velocity;
@@ -41,14 +57,13 @@ class MyVehicle extends CGFobject {
         }
     }
     turn(val) {
-        this.angleY += (2*Math.PI)/125;
         this.angleY += val;
     }
     leme(i) {
         switch (i) {
             case 0:
                 this.zeppelin.updateLemes(-Math.PI/12, -1);
-                break;
+                return;
             case 1:
                 this.zeppelin.updateLemes(Math.PI/12, 1);
                 return;
