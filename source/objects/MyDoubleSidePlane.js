3f44a73b5e9984ce568f0dd62af2f636c3b44642
Hugomguima <50011557+Hugomguima@users.noreply.github.com>
2020-05-10 19:20:54 +0100

    Plane changed to double side

--- source/objects/MyDoubleSidePlane.js
+++ source/objects/MyDoubleSidePlane.js
@@ -1,71 +1,21 @@
 
 /** Represents a plane with nrDivs divisions along both axis, with center at (0,0) */
 class MyDoubleSidePlane extends CGFobject{
-	constructor(scene, nrDivs, minS, maxS, minT, maxT) {
+	constructor(scene,number) {
 		super(scene);
-		// nrDivs = 1 if not provided
-		nrDivs = typeof nrDivs !== 'undefined' ? nrDivs : 1;
-		this.nrDivs = nrDivs;
-		this.patchLength = 1.0 / nrDivs;
-		this.minS = minS || 0;
-		this.maxS = maxS || 1;
-		this.minT = minT || 0;
-		this.maxT = maxT || 1;
-		this.q = (this.maxS - this.minS) / this.nrDivs;
-		this.w = (this.maxT - this.minT) / this.nrDivs;
-		this.initBuffers();
+		this.plane = new MyPlane(scene,number);
 	}
-	/**
-   * @method initBuffers
-   * Initializes Plane's buffers
-   */
-	initBuffers() {
-		// Generate vertices, normals, and texCoords
-		this.vertices = [];
-		this.normals = [];
-		this.texCoords = [];
-		var yCoord = 0.5;
-		for (var j = 0; j <= this.nrDivs; j++) {
-			var xCoord = -0.5;
-			for (var i = 0; i <= this.nrDivs; i++) {
-				this.vertices.push(xCoord, yCoord, 0);
-				this.normals.push(0, 0, 1);
-				this.texCoords.push(this.minS + i * this.q, this.minT + j * this.w);
-				xCoord += this.patchLength;
-			}
-			yCoord -= this.patchLength;
-		}
-		// Generating indices
-		this.indices = [];
 
-		var ind = 0;
-		for (var j = 0; j < this.nrDivs; j++) {
-			for (var i = 0; i <= this.nrDivs; i++) {
-				this.indices.push(ind);
-				this.indices.push(ind + this.nrDivs + 1);
-				ind++;
-			}
-			if (j + 1 < this.nrDivs) {
-				this.indices.push(ind + this.nrDivs);
-				this.indices.push(ind);
-			}
-		}
-		this.primitiveType = this.scene.gl.TRIANGLE_STRIP;
-		this.initGLBuffers();
+	display(){
+		this.scene.pushMatrix();
+		this.plane.display();
+		this.scene.popMatrix();
+
+		this.scene.pushMatrix();
+		this.scene.rotate(Math.PI,0,1,0);
+		this.plane.display();
+		this.scene.popMatrix();
 	}
-	/**
-	 * Set Method to enable Fill mode (Uses triangles)
-	 */
-	setFillMode() { 
-		this.primitiveType=this.scene.gl.TRIANGLE_STRIP;
-	}
-	/**
-	 * Set Method to enable Line mode (Uses lines)
-	 */
-	setLineMode() 
-	{ 
-		this.primitiveType=this.scene.gl.LINES;
-	};
 
 }
 
